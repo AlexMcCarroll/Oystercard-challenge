74830882550c1fe69dc3d00cf00de6f30ddd8b7e
Gia1987 <giacomo.n27@gmail.com>
2018-02-26 16:17:34 +0000

    Add the balance step 4

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,3 +1,11 @@
-describe Oystecard do
-  
+require 'oystercard'
+describe Oystercard do
+
+  describe '#balance' do
+    it {is_expected.to respond_to :balance}
+    it 'has a balance eq to 0' do
+      expect(subject.balance).to eq 0
+    end
+  end
+
 end
