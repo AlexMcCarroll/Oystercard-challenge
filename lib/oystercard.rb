e58e3b37be1d793615b59522df70f4e1aa920fde
Raefe <raefe.nj@gmail.com>
2018-02-28 12:17:43 +0000

    raefes grumpy refactoring

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,14 +1,11 @@
 # This is the Oystercard class
 class Oystercard
   attr_reader :balance, :limit, :in_station, :entry_station, :exit_station
-  ZERO = 0
-  ONE = 1
   LIMIT = 90
   FARE = 1
-  def initialize(balance = ZERO, limit = LIMIT)
+  def initialize(balance = 0, limit = LIMIT)
     @balance = balance
     @limit = limit
-    # @in_station = false
     @entry_station = nil
     @exit_station = nil
   end
@@ -19,26 +16,21 @@ class Oystercard
   end
 
   def touch_in(station)
-    raise 'Sorry you need to top up' if minimal
-    # @in_station = true
+    raise 'Sorry you need to top up' if @balance < FARE
     @entry_station = station
   end
 
   def touch_out(station)
     deduct_fare(FARE)
-    # @in_station = false
+    @entry_station = nil
     @exit_station = station
   end
 
-  def in_journey?(station)
-    @entry_station = station
+  def in_journey?
+    @entry_station != nil
   end
 
   private
-
-  def minimal
-    @balance < ONE
-  end
 
   def limit(pound)
     (@balance + pound) > LIMIT
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -2,45 +2,43 @@ require 'oystercard'
 describe Oystercard do
   subject(:Oystercard) { Oystercard.new }
   let(:fake_station) { double :station }
+  limit = Oystercard::LIMIT
+  fare = Oystercard::FARE
 
   describe '#balance' do
-    it { is_expected.to respond_to :balance }
-    it "has a balance eq to #{Oystercard::ZERO}" do
-      expect(subject.balance).to eq Oystercard::ZERO
+    it "has a balance eq to 0" do
+      expect(subject.balance).to eq 0
     end
   end
   describe '#top_up' do
-    it 'increase the balance of the card' do
-      expect(subject).to respond_to(:top_up).with(Oystercard::ONE).argument
-    end
     it 'returns the balance from top_up' do
-      expect { subject.top_up Oystercard::ONE }.to change { subject.balance }.by Oystercard::ONE
+      expect { subject.top_up(1) }.to change { subject.balance }.by 1
     end
-    it "raises an error message if new balance > #{Oystercard::LIMIT} pounds" do
-      expect { subject.top_up(Oystercard::LIMIT + 1) }.to raise_error 'Sorry the new balance would exceed the limit!'
+    it "raises an error message if new balance > #{limit} pounds" do
+      expect { subject.top_up(limit + 1) }.to raise_error 'Sorry the new balance would exceed the limit!'
     end
   end
 
   describe '#touch_in' do
-    it "raises an error message if balance is less than #{Oystercard::ONE} pound" do
-      expect(subject.balance).to be < Oystercard::ONE
+    it "raises an error message if balance is less than #{1} pound" do
+      expect(subject.balance).to be < fare
       expect { subject.touch_in(fake_station) }.to raise_error(RuntimeError)
     end
     it 'the card has touched in' do
-      subject.top_up(Oystercard::ONE)
+      subject.top_up(fare)
       expect(subject.touch_in(fake_station)).to eq fake_station
     end
     it 'remembers touch in station' do
-      subject.top_up(Oystercard::ONE)
+      subject.top_up(fare)
       subject.touch_in(fake_station)
       expect(subject.entry_station).to eq fake_station
     end
   end
 
   describe '#touch_out' do
-    it "deducts #{Oystercard::FARE} from the card when touched out" do
-      subject.top_up(Oystercard::ONE)
-      expect { subject.touch_out(fake_station) }.to change { subject.balance }.by(-Oystercard::FARE)
+    it "deducts #{fare} from the card when touched out" do
+      subject.top_up(fare)
+      expect { subject.touch_out(fake_station) }.to change { subject.balance }.by(-fare)
     end
     it 'the card has touched out' do
       expect(subject.touch_out(fake_station)).to eq fake_station
@@ -53,7 +51,9 @@ describe Oystercard do
 
   describe '#in_journey?' do
     it 'the card has touched in and not touched out yet' do
-      expect(subject.in_journey?(fake_station)).to eq fake_station
+      subject.top_up(fare)
+      subject.touch_in(fake_station)
+      expect(subject.in_journey?).to eq true
     end
   end
 end
