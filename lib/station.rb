2696e65106d63c19154215da46242b94e2a2ebca
Raefe <raefe.nj@gmail.com>
2018-02-28 14:45:40 +0000

    some patronising coding wtih raefey

--- lib/station.rb
+++ lib/station.rb
@@ -1,4 +1,11 @@
 # This is the station class
 class Station
 
+  attr_reader :name, :zone
+
+  def initialize(station_info)
+    @name = station_info[:name]
+    @zone = station_info[:zone]
+
+  end
 end
--- spec/station_spec.rb
+++ spec/station_spec.rb
@@ -1,6 +1,19 @@
 require 'station'
 
 describe Station do
-  subject(:Station) { Station.new }
+
+  subject {described_class.new(name: "Aldgate East", zone: 1)}
+
+  describe "station name stored" do
+    it "reads the station name" do
+    expect(subject.name).to eq("Aldgate East")
+  end
+end
+
+  describe "station name stored" do
+    it "reads the station zone" do
+    expect(subject.zone).to eq(1)
+  end
+end
 
 end
